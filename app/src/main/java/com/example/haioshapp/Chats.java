acf53bef0cf70a0d58db533a7672eb83b0a103f7
Hadas Eshel Sofer <hadseshel@gmail.com>
2022-06-12 15:49:02 +0300

    create login register base chat

--- app/src/main/java/com/example/haioshapp/Chats.java
+++ app/src/main/java/com/example/haioshapp/Chats.java
@@ -1,7 +1,9 @@
 package com.example.haioshapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,5 +29,13 @@ public class Chats extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,list);
         listView.setAdapter(arrayAdapter);
+
+        // pass the add chat screen
+        ImageButton btn_add_chat = findViewById(R.id.add_chat_icon);
+        btn_add_chat.setOnClickListener(v->{
+            // need to add validazia
+            Intent intent = new Intent(this,NewChat.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/haioshapp/SingleChat.java
+++ app/src/main/java/com/example/haioshapp/SingleChat.java
@@ -1,14 +1,30 @@
 package com.example.haioshapp;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SingleChat extends AppCompatActivity {
+    List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_chat);
+
+        list = new ArrayList<String>();
+        list.add("hi");
+        list.add("how are you?");
+        list.add("fine");
+
+        ListView listView = findViewById(R.id.singlechat_listview);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(arrayAdapter);
     }
 }
