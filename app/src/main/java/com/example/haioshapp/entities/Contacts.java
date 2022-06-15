4535af3380d86ab39b3566b0a18a2b307408834c
Hadas Eshel Sofer <hadseshel@gmail.com>
2022-06-15 11:44:28 +0300

    create login register base chat

---
+++ app/src/main/java/com/example/haioshapp/entities/Contacts.java
@@ -0,0 +1,8 @@
+package com.example.haioshapp.entities;
+
+public class Contacts {
+    private String name;
+    public String getName() {
+        return name;
+    }
+}
---
+++ app/src/main/java/com/example/haioshapp/entities/Message.java
@@ -0,0 +1,5 @@
+package com.example.haioshapp.entities;
+
+public class Message {
+
+}
